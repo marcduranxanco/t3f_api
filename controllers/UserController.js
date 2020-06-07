a1bbb96eef93b936bf3cb532945bdc82ba267e2c
MarcDuran <marcduranxanco@hotmail.com>
2020-06-07 19:13:14 +0200

    CREATE: Crud users init commit

---
+++ controllers/UserController.js
@@ -0,0 +1,9 @@
+const { User } = require('../database/models');
+
+const UserController = {
+    signin(req, res){
+        console.log("Test Signin method")
+    }
+}
+
+module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController'); //Import User Controller
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST */
+router.post('/signin', UserController.signin);
 
 module.exports = router;
