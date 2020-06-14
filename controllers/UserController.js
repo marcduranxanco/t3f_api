8caa0d085dc3972cc9ec88616c6ce5da4aef5185
MarcDuran <marcduranxanco@hotmail.com>
2020-06-14 22:38:54 +0200

    UPDATE: Crud users init

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,30 @@
-const { User } = require('../database/models');
+const { users } = require('../database/models');
 
 const UserController = {
-    signin(req, res){
-        console.log("Test Signin method")
-    }
-}
+
+    /* CRUD USERS */
+//Create
+    create(req, res) {
+        // users.create(req.body)
+        // .then(user => res.status(201).send(user))
+        // .catch(error => {
+        //     console.error(error);
+        //     res.status(500).send({
+        //         message: `There was a problem trying to register the user`
+        //     })
+        // });
+        res.send('Create');
+    },
+//Read
+    getAll(req, res, next) { res.send('Get all')},
+    getOne(req, res, next) { res.send('Get one')},
+//Update
+    update(req, res, next) { res.send('update')},
+//Delete
+    delete(req, res, next) { res.send('delete')}
+
+/* LOGIN */
+
+};
 
 module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController'); //Import User Controller
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* CRUD USERS */
+//Create
+router.post('/', UserController.create);
+//Read
+router.get('/', UserController.getAll);
+router.get('/:id', UserController.getOne);
+//Update
+router.put('/:id', UserController.update);
+//Delete
+router.delete('/:id', UserController.delete);
 
-/* POST */
-router.post('/signin', UserController.signin);
+/* LOGIN */
 
 module.exports = router;
