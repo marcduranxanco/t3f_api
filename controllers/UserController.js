8acacb60e03f58d5c715ae539f2133382b40028b
MarcDuran <marcduranxanco@hotmail.com>
2020-06-15 22:25:20 +0200

    UPDATE: Register or user creation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,19 +1,20 @@
 const { users } = require('../database/models');
+const bcrypt = require('bcryptjs');
 
 const UserController = {
 
     /* CRUD USERS */
 //Create
     create(req, res) {
-        // users.create(req.body)
-        // .then(user => res.status(201).send(user))
-        // .catch(error => {
-        //     console.error(error);
-        //     res.status(500).send({
-        //         message: `There was a problem trying to register the user`
-        //     })
-        // });
-        res.send('Create');
+        req.body.password = bcrypt.hashSync(req.body.password, 10); //Encrypt the user password
+        users.create(req.body)
+        .then(user => res.status(201).send(user))
+        .catch(error => {
+            console.error(error);
+            res.status(500).send({
+                message: `There was a problem trying to register the user`
+            })
+        });
     },
 //Read
     getAll(req, res, next) { res.send('Get all')},
@@ -21,9 +22,13 @@ const UserController = {
 //Update
     update(req, res, next) { res.send('update')},
 //Delete
-    delete(req, res, next) { res.send('delete')}
-
-/* LOGIN */
+    delete(req, res, next) { res.send('delete')},
+    
+    /* LOGIN */
+    login(req, res, next) { 
+        
+    }
+    
 
 };
 
