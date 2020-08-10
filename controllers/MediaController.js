269f8677a0147a381bb4eda36b7fa99856f1b9a1
MarcDuran <marcduranxanco@hotmail.com>
2020-08-10 23:47:26 +0200

    UPDATE: Create media

--- controllers/MediaController.js
+++ controllers/MediaController.js
@@ -1,4 +1,4 @@
-const { media } = require("../database/models");
+const { Media } = require("../database/models");
 
 //CRUD MEDIA
 /* MEDIACONTROLLER DEFINITION */
@@ -7,28 +7,27 @@ const MediaController = {};
 //CREATE: required params: id_tmdb, id_custom_media, id_platform
 MediaController.create = async (req, res) => {
     //CHECK PLATFORMS
-    if(IsJsonString(req.body.platforms)){
-        let platforms = JSON.parse(req.body.platforms);
-        console.log(platforms);
+    // if(IsJsonString(req.body.platforms)){
+    //     let platforms = JSON.parse(req.body.platforms); //Futur
+    //     //INSERT THE MEDIA ELEMENT
         
-        
-    }else{
-        res.status(500).send({
-            message: `There was a problem creating a new media`,
-            error: `The param "platforms" it's not a json`,
-        });
-    }
-
-    // media
-    //     .create(req.body)
-    //     .then((media) => res.status(201).send(media))
-    //     .catch((err) => {
-    //         console.error(err);
-    //         res.status(500).send({
+    // }else{
+    //     res.status(500).send({
     //         message: `There was a problem creating a new media`,
-    //         error: err.message,
-    //         });
+    //         error: `The param "platforms" it's not a json`,
     //     });
+    // }
+
+    Media
+        .create(req.body)
+        .then((media) => res.status(201).send(media))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send({
+            message: `There was a problem creating a new media`,
+            error: err.message,
+            });
+        });
 };
 
 function IsJsonString(str) {
@@ -42,7 +41,21 @@ function IsJsonString(str) {
 
 //Update
 MediaController.read = async (req, res) => {
-    res.status(200).send("read");
+    console.log("test");
+    //READ ALL
+    if (!req.params.id) {
+        Media.findAll().then((media) => {
+        res.status(200).json(media);
+        });
+        //READ ONE
+    } else {
+        let media = await Media.findByPk(req.params.id);
+        if (!media) {
+        return res.status(400).send({ message: "This media you have requested doesn't exists" });
+        } else {
+        return res.status(200).send(media);
+        }
+    }
 };
 
 //Read
