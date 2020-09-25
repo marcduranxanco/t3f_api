aa49f266c456303ac213f91cc29894208f812d38
MarcDuran <marcduranxanco@hotmail.com>
2020-09-25 14:00:34 +0200

    ADD: middleware routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const CardsController = require('../controllers/CardsController'); //Import Cards Controller
+const {authentication} = require('../middleware/authentication.js');
 
 /* CRUD Cards */
 //CREATE
-router.post('/', CardsController.create);
+router.post('/', authentication, CardsController.create);
 //READ
 router.get('/:id?', CardsController.read);
 //UPDATE
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController'); //Import User Controller
+const {authentication, isAdmin} = require('../middleware/authentication.js');
 
 /* CRUD USERS */
 //CREATE
 router.post('/', UserController.create);
 //READ
-router.get('/:id?', UserController.read);
+router.get('/:id?', authentication, isAdmin, UserController.read);
 //UPDATE
 router.put('/:id', UserController.update);
 //DELETE
