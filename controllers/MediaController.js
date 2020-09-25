002008897b75a16959451c307b4c9184ed440409
MarcDuran <marcduranxanco@hotmail.com>
2020-09-25 14:00:18 +0200

    ADD: middleware for media queries

--- controllers/MediaController.js
+++ controllers/MediaController.js
@@ -74,22 +74,21 @@ function IsJsonString(str) {
 
 //Update
 MediaController.read = async (req, res) => {
-  //READ ALL
-  if (!req.params.id) {
-    Media.findAll().then((media) => {
-      res.status(200).json(media);
-    });
-    //READ ONE
+  //READ ONE
+  let media = await Media.findByPk(req.params.id);
+  if (!media) {
+    return res
+      .status(400)
+      .send({ message: "This media you have requested doesn't exists" });
   } else {
-    let media = await Media.findByPk(req.params.id);
-    if (!media) {
-      return res
-        .status(400)
-        .send({ message: "This media you have requested doesn't exists" });
-    } else {
-      return res.status(200).send(media);
-    }
+    return res.status(200).send(media);
   }
+};
+MediaController.readAll = async (req, res) => {
+  //READ ALL
+  Media.findAll().then((media) => {
+    res.status(200).json(media);
+  });
 };
 
 //Read
--- routes/media.js
+++ routes/media.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const MediaController = require('../controllers/MediaController'); //Import Media Controller
+const {authentication, isAdmin} = require('../middleware/authentication.js');
 
 /* CRUD MEDIA */
 //CREATE
 router.post('/', MediaController.create);
 //READ
-router.get('/:id?', MediaController.read);
+router.get('/:id', MediaController.read);
+router.get('/:all', authentication, isAdmin, MediaController.readAll);
 //UPDATE
 router.put('/:id', MediaController.update);
 //DELETE
