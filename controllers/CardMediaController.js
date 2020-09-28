d821e146befdc60db62cb8ba62d343d10fe4acff
MarcDuran <marcduranxanco@hotmail.com>
2020-09-28 23:15:37 +0200

    UPDATE: CardMedia crud

--- controllers/CardMediaController.js
+++ controllers/CardMediaController.js
@@ -1,4 +1,4 @@
-const { cards_media } = require("../database/models");
+const { Cards_Media } = require("../database/models");
 
 //CRUD MEDIA
 /* CARDSCONTROLLER DEFINITION */
@@ -6,25 +6,76 @@ const CardMediaController = {};
 
 //CREATE: required params: id_tmdb, id_custom_media, id_platform
 CardMediaController.create = async (req, res) => {
-    res.status(200).send("Create cardmedia");
+    Cards_Media
+    .create(req.body)
+    .then((cardMedia) => res.status(201).send(cardMedia))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send({
+        message: `There was a problem trying to register the cardMedia media`,
+        error: err.message,
+      });
+    });
 };
-
 
 //Update
 CardMediaController.read = async (req, res) => {
-    res.status(200).send("Read cardmedia");
+    //READ ALL
+    if (!req.params.id) {
+        Cards_Media.findAll().then((cardMedia) => {
+        res.status(200).json(cardMedia);
+        });
+    //READ ONE
+    } else {
+        let cardMedia = await Cards_Media.findByPk(req.params.id);
+        if (!cardMedia) {
+        return res.status(400).send({ message: "This cardMedia doesn't exists" });
+        } else {
+        return res.status(200).send(cardMedia);
+        }
+    }
 };
 
 //Read
 // Lectura de todos los datos
 CardMediaController.update = async (req, res) => {
-    res.status(200).send("Update cardmedia");
+    let cM = req.body;
+    await Cards_Media
+      .findOne({
+        where: { id: req.params.id },
+      })
+      .then((card) => {
+        card.update(cM);
+        card.save().then(() => {
+          res.status(200).send({
+            message: "Cm updated correctly",
+          });
+        }).catch((err) => {
+          res.status(400).send({
+            message: "Error updating cm",
+            error: err.message,
+          });
+        });
+      })
 };
 
 //Delete
 //Es necesario que haga un delete?
 CardMediaController.delete = async (req, res) => {
-    res.status(200).send("Delete cardmedia");
+    await Cards_Media
+    .findByPk(req.params.id)
+    .then((cM) => {
+      cM.destroy();
+      res.status(200).send({
+        message: "cM deleted correctly",
+      });
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: "Error deleting cM",
+        error: err.message,
+      });
+    });
 };
 
 module.exports = CardMediaController;
