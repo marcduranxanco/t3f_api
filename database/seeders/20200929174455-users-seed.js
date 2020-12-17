3d0288bc024663e1f40e214974eab4301b8bcb6c
MarcDuran <marcduranxanco@hotmail.com>
2020-12-17 21:37:44 +0100

    UPDATE: Users seed

--- database/seeders/20200929174455-users-seed.js
+++ database/seeders/20200929174455-users-seed.js
@@ -4,46 +4,26 @@ const bcrypt = require("bcryptjs");
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     let pwd = bcrypt.hashSync(process.env.DEFAULT_USERS_SECRET, 10);
 
     return queryInterface.bulkInsert('Users', [
       {
-      user_name: 'User1',
-      name: 'User1',
-      surname: 'Sur1',
-      email: 'User1@example.com',
+      user_name: 'Superadmin',
+      name: 'Superadmin',
+      surname: 'User Super',
+      email: 'superadmin@t3f.dev',
       password: pwd,
       role: 'superAdmin',
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
-      user_name: 'User2',
-      name: 'User2',
-      surname: 'Sur2',
-      email: 'User2@example.com',
+      user_name: 'Admin',
+      name: 'Admin',
+      surname: 'User Admin',
+      email: 'admin@t3f.dev',
       password: pwd,
       role: 'admin',
-      createdAt: new Date(),
-      updatedAt: new Date()
-      },
-      {
-      user_name: 'User3',
-      name: 'User3',
-      surname: 'Sur3',
-      email: 'User3@example.com',
-      password: pwd,
-      role: 'user',
       createdAt: new Date(),
       updatedAt: new Date()
       }
@@ -51,13 +31,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
     return queryInterface.bulkDelete('Users', null, {});
   }
 };
